e77e3fee003ba14fcfad43b55067eca0aa88b5f2
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-08-15 11:45:48 +0800

    Add files via upload

--- img_process.py
+++ img_process.py
@@ -10,6 +10,13 @@ import numpy as np
 import copy
 import random as rng
 import os
+import shutil
+if os.path.exists("./mask"):
+    shutil.rmtree('./mask')
+if os.path.exists("./roi"):
+    shutil.rmtree('./roi')
+if os.path.exists("./roi_2"):   
+    shutil.rmtree('./roi_2')
 #======================================================================================================
 #Before start coding~~~~~~~~~~~~~~~~~~~~~~
 #========================Study image processing code===================================================
@@ -32,11 +39,11 @@ import os
 #========================
 #Step 0 :loading image
 #========================
-raw_img = cv2.imread('./datasets/landmark_img/8526a3b2-b5317e10.jpg')
-img = cv2.imread('./datasets/landmark_img/8526a3b2-b5317e10.jpg')
+raw_img = cv2.imread('./datasets/landmark_img/Screenshot from 2023-08-15 10-55-04.png')
+img = cv2.imread('./datasets/landmark_img/Screenshot from 2023-08-15 10-55-04.png')
 # 查看資料型態
 print(type(img))
-print(img.shape)
+#print(img.shape)
 
 img_h = img.shape[0]
 img_w = img.shape[1]
@@ -60,7 +67,7 @@ blurred = cv2.GaussianBlur(img, (5, 5), 0)
 edge = cv2.Canny(blurred, t_lower, t_upper, L2gradient = L2Gradient ) #Get Canny image
 
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Convert BGR to Gray image
-ret, thresh = cv2.threshold(imgray, 127, 255, 0) #imput Gray image, output Binary images (Mask)
+ret, thresh = cv2.threshold(imgray, 170, 255, 0) #imput Gray image, output Binary images (Mask)
 
 kernel = np.ones((3, 3), np.uint8)
   
@@ -78,13 +85,13 @@ contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_S
 #cv2.drawContours(img, contours, -1, (0,255,0), 3)
 
 mask = np.zeros(img.shape, np.uint8)
-for c in contours:
-    color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
+# for c in contours:
+#     color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
     
-    area = cv2.contourArea(c)
-    print("area: {}".format(area))
-    if area > 400:
-        cv2.drawContours(mask, c, -1, color , cv2.CHAIN_APPROX_SIMPLE)
+#     area = cv2.contourArea(c)
+#     print("area: {}".format(area))
+#     if area > 400:
+#         cv2.drawContours(mask, c, -1, color , cv2.CHAIN_APPROX_SIMPLE)
 
 contours_poly = [None]*len(contours)
 boundRect = [None]*len(contours)
@@ -102,9 +109,12 @@ drawing = np.ones((edge.shape[0], edge.shape[1], 3), dtype=np.uint8)
 #========================================
 #Create folders
 #========================================
+#os.system("rm -rf /roi")
+#os.system("rm -rf /mask")
+#os.system("rm -rf /roi_2")
 os.makedirs("./mask",exist_ok=True)
 os.makedirs("./roi",exist_ok=True)
-os.makedirs("./roi_2",exist_ok=True)
+#os.makedirs("./roi_2",exist_ok=True)
 #==================================
 #Step 5 :  Go through  all contours
 #===================================
@@ -148,34 +158,36 @@ for i in range(len(contours)):
         #================================
         # Save roi without background
         #=================================
-        roi_2 = raw_img[y:y+h, x:x+w]
-        roi_2[binary==0] = 0 
-        cv2.imwrite("roi_2/"+str(i)+".jpg", roi_2)
+        # roi_2 = raw_img[y:y+h, x:x+w]
+        # roi_2[binary==0] = 0 
+        # cv2.imwrite("roi_2/"+str(i)+".jpg", roi_2)
         
         #========================================================
         #Draw the final filtered contours with bounding boxes
         #=========================================================
         cv2.drawContours(img, contours_poly, i%255 , cv2.CHAIN_APPROX_SIMPLE)
-        cv2.rectangle(img, (int(boundRect[i][0]), int(boundRect[i][1])), \
-    (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 6)
+        
      
     #cv2.circle(drawing, (int(centers[i][0]), int(centers[i][1])), int(radius[i]), color, 1)
-
+    if w*h > small_size and w*h < large_size and y> (edge.shape[0]*1.0/5.0) \
+        and float(w/h)>small_r and float(w/h)<large_r:
+        cv2.rectangle(img, (int(boundRect[i][0]), int(boundRect[i][1])), \
+        (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 6)
 
 print("im gray {}".format(imgray.shape))
 
-os.makedirs("./img", exist_ok=True)
-os.makedirs("./Mask", exist_ok=True)
-os.makedirs("./blur", exist_ok=True)
-os.makedirs("./Gary", exist_ok=True)
-os.makedirs("./Canny", exist_ok=True)
-os.makedirs("./Binary",exist_ok=True)
+# os.makedirs("./img", exist_ok=True)
+# os.makedirs("./Mask", exist_ok=True)
+# os.makedirs("./blur", exist_ok=True)
+# os.makedirs("./Gary", exist_ok=True)
+# os.makedirs("./Canny", exist_ok=True)
+# os.makedirs("./Binary",exist_ok=True)
 
-cv2.imwrite("./img/img.jpg",img)
-cv2.imwrite("./Binary/Binary.jpg",thresh)
-cv2.imwrite("./Canny/Canny.jpg",edge)
-cv2.imwrite("./blur/blur.jpg",blurred)
-cv2.imwrite("./Gray/Gray.jpg",imgray)
+# cv2.imwrite("./img/img.jpg",img)
+# cv2.imwrite("./Binary/Binary.jpg",thresh)
+# cv2.imwrite("./Canny/Canny.jpg",edge)
+# cv2.imwrite("./blur/blur.jpg",blurred)
+# cv2.imwrite("./Gray/Gray.jpg",imgray)
 
 #========================================
 #Visualize the processed images
